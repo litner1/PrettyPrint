034d606c1a9e876cb741818880f2614d335a38f6
litner1 <igorlitner@wp.pl>
2025-01-28 23:19:08 +0100

    adjusting name and README

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-# prettyprint README
+# PrettyPrintXML README
 
-This extension replaces &lt; &gt; and &amp; then prettyprints
+This extension replaces "& lt;", "& gt;" and "& amp;" then prettyprints XML.
 
-Press alt+shift+f to prettyprint XML file
+Press alt+shift+f to prettyprint XML file.
 
+https://github.com/litner1/VSCode-XML-extension
--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	//console.log('Congratulations, your extension "prettyprint" is now active!');
 
-	const disposable = vscode.commands.registerCommand('prettyprint', () => {
+	const disposable = vscode.commands.registerCommand('PrettyPrintXML', () => {
 		const editor = vscode.window.activeTextEditor;
 		if (!editor) {
 			vscode.window.showErrorMessage('No active editor! Open a file to use this command.');
